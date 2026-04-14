0570e7d89732cacd8c6cef742dd03c305a1e5555
Priyank Shukla <umeshusha1974@gmail.com>
2026-04-14 09:46:03 +0530

    update CORS configuration and cookie options for improved security and compatibility

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -9,7 +9,7 @@ import { errorHandler } from './src/middleware/errorHandler.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:5173'] ,
+app.use(cors({ origin: ['http://localhost:5173', 'https://your-app.vercel.app'] ,
     credentials: true
 })); 
 app.use(express.json());
--- BACKEND/src/config/config.js
+++ BACKEND/src/config/config.js
@@ -1,6 +1,6 @@
 export const cookiesOptions = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge:  5 * 60 * 1000, // 7 days
+    secure:true,
+    sameSite: "none",
+    maxAge:  7 * 24 * 60 * 60 * 1000, // 7 days
 }
